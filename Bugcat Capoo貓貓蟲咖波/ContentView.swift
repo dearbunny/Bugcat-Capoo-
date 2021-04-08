2e8c77aec605845906c600c149c652fbba38470e
Rose <qqbunny@icloud.com>
2021-04-08 16:02:09 +0800

    Initial Commit

---
+++ Bugcat Capoo貓貓蟲咖波/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bugcat Capoo貓貓蟲咖波
+//
+//  Created by Rose on 2021/4/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
